ab92702c6d7cecf58e382ef0bd84a4385041e596
Maruf <mamaruf317@gmail.com>
2023-10-29 21:34:57 +0600

    maruf_bro

---
+++ TwoArrays.cpp
@@ -0,0 +1,67 @@
+/*maruf_bro*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ld long double
+#define ll long long
+#define vec vector<ll>
+#define pb push_back
+#define fcin(n)       \
+    for (auto &x : n) \
+    cin >> x
+#define fcout(n)     \
+    for (auto x : n) \
+    cout << x << " "
+#define YES puts("YES")
+#define NO puts("NO")
+#define endl '\n'
+/*
+ "The harder the problem, the sweeter the victory. Keep coding!"
+*/
+
+int Case = 1;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vec v1(n), v2(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v1[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v2[i];
+    }
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    for (int i = 0; i < n; i++)
+    {
+
+        if (v1[i] > v2[i] || v2[i] - v1[i] > 1)
+        {
+            NO;
+            return;
+        }
+    }
+
+    YES;
+}
+
+signed main()
+{
+    IO;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
