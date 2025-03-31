e0feff8b859ef453839d86beb9b38a2b13d249b5
Maruf <mamaruf317@gmail.com>
2025-03-31 20:03:44 +0600

    maruf_bro

---
+++ A_Kamilka_and_the_Sheep.cpp
@@ -0,0 +1,49 @@
+/*maruf_bro*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define IO                            \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define ld long double
+#define ll long long
+#define vec vector<ll>
+#define pb push_back
+#define fcin(n)       \
+    for (auto &x : n) \
+    cin >> x
+#define fcout(n)     \
+    for (auto x : n) \
+    cout << x << " "
+#define YES puts("YES")
+#define NO puts("NO")
+#define endl '\n'
+/*
+ "The harder the problem, the sweeter the victory. Keep coding!"
+*/
+
+int Case = 1;
+
+void solve()
+{
+    ll n; 
+	cin >> n;
+    vec a(n);
+    for (ll i = 0; i < n; i++) cin >> a[i];
+    sort(a.begin(), a.end());
+    cout << a[n - 1] - a[0] << endl;
+}
+
+signed main()
+{
+    IO;
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
